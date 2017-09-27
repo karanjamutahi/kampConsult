f979f56ff2c775e7ce93526b029f683ab212c69d
Karanja Mutahi <mutahi.henry@gmail.com>
2017-09-27 22:26:30 +0300

    Ready for initial staging
    Not seen how it looks on mobile

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
     console.log("Ready!");
 
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > 700){
+            $("#topNav").each(function(){
+                this.style.setProperty('background','solid', 'important');
+                this.style.setProperty('background-color','white', 'important');
+            });
+            
+        }
+        else{
+            $("#topNav").each(function(){
+                this.style.setProperty('background','transparent', 'important');
+                this.style.setProperty('background-color','rgba(0,0,0,0)', 'important');
+            });
+        }
+    });
 });
